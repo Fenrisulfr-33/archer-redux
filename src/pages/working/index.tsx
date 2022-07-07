ef1a9c46e81624f40f6f9a2dadc6b090d6e0d793
Fenrisulfr.33 <Fenrisulfr.33@protonmail.com>
2022-07-07 14:56:55 -0400

    Inital project setup

---
+++ src/pages/working/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Redux Toolkit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className='bg-blue-400'>
+
+      </header>
+
+        <p className={'bg-sky-400'}>hey</p>
+ 
+    </>
+  )
+}
+
+export default IndexPage
