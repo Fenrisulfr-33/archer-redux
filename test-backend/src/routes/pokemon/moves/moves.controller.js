3c22ab2f1c0e0c3722b29de6c6dd6549776a3f74
Fenrisulfr.33 <Fenrisulfr.33@protonmail.com>
2023-02-20 21:26:56 -0500

    in progress changes

--- test-backend/src/routes/pokemon/moves/moves.controller.js
+++ test-backend/src/routes/pokemon/moves/moves.controller.js
@@ -1,24 +1,13 @@
- // This lets use use try catch without always have to catch an error
- const asyncHandler = require('express-async-handler');
- const Moves = require('../../../models/pokemon/movesModel');
- const { connect, disconnect } = require('../connection');
- const local = process.env.ENV === 'local' ? true : false;
- /**
-  *  lists all pokemon in order of sword and shield dex number
-  * 
-  * @returns {JSON}
-  *  The base sword and shield dex
-  */
- const listMoves = asyncHandler(async (request, response) => {
-     if (local) {
+const asyncHandler = require('express-async-handler');
+const Moves = require('../../../models/pokemon/movesModel');
+const { connect, disconnect } = require('../connection');
 
-     } else {
-        const moves = await Moves.find().select('name.english type category pp power accuracy').sort({ _id: 1 }); // just for the list view on the natioanl view page
-         disconnect();
-         response.status(200).json(moves);
-     }
- });
- 
- module.exports = {
-     list: [connect, listMoves]
- }
+const listMoves = asyncHandler(async (request, response) => {
+    const moves = await Moves.find().select('name.english type category pp power accuracy').sort({ _id: 1 }); // just for the list view on the natioanl view page
+    disconnect();
+    response.status(200).json(moves);
+});
+
+module.exports = {
+    list: [connect, listMoves]
+}
--- test-backend/src/routes/pokemon/search/search.controller.js
+++ test-backend/src/routes/pokemon/search/search.controller.js
@@ -1,68 +1,41 @@
- // This lets use use try catch without always have to catch an error
- const asyncHandler = require('express-async-handler');
- const National = require('../../../models/pokemon/nationalModel');
- const { connect, disconnect } = require('../connection');
+// This lets use use try catch without always have to catch an error
+const asyncHandler = require('express-async-handler');
+const National = require('../../../models/pokemon/nationalModel');
+const { connect, disconnect } = require('../connection');
 
- const searchPokemon = asyncHandler(async (request, response) => {
+const searchPokemon = asyncHandler(async (request, response) => {
     const {
-        move1, 
-        move2, 
-        move3, 
+        move1,
+        move2,
+        move3,
         move4,
         // game,
     } = request.query;
     const game = 'scarlet-violet';
-    if (move4){
-        const found = await National
+    const gameList = await National
         .find()
-        .where(`moves.${move1}.${game}-lvl` || `moves.${move1}.${game}-machine` ||`moves.${move1}.${game}-tutor`)
+        .where(`moves.${game}`)
         .exists(true)
-        .where(`moves.${move2}.${game}-lvl` || `moves.${move2}.${game}-machine` ||`moves.${move2}.${game}-tutor`)
-        .exists(true)
-        .where(`moves.${move3}.${game}-lvl` || `moves.${move3}.${game}-machine` ||`moves.${move3}.${game}-tutor`)
-        .exists(true)
-        .where(`moves.${move4}.${game}-lvl` || `moves.${move4}.${game}-machine` ||`moves.${move4}.${game}-tutor`)
-        .exists(true)
-        .select('name.english type abilities baseStats')
+        // .select('name.english type abilities baseStats')
         .sort({ _id: 1 });
-        disconnect();
-        response.status(200).json(found);
-    } else if (move3){
-        const found = await National
-        .find()
-        .where(`moves.${move1}.${game}-lvl` || `moves.${move1}.${game}-machine` ||`moves.${move1}.${game}-tutor`)
-        .exists(true)
-        .where(`moves.${move2}.${game}-lvl` || `moves.${move2}.${game}-machine` ||`moves.${move2}.${game}-tutor`)
-        .exists(true)
-        .where(`moves.${move3}.${game}-lvl` || `moves.${move3}.${game}-machine` ||`moves.${move3}.${game}-tutor`)
-        .exists(true)
-        .select('name.english type abilities baseStats')
-        .sort({ _id: 1 });
-        disconnect();
-        response.status(200).json(found);
-    } else if (move2){
-        const found = await National
-        .find()
-        .where(`moves.${move1}.${game}-lvl` || `moves.${move1}.${game}-machine` ||`moves.${move1}.${game}-tutor`)
-        .exists(true)
-        .where(`moves.${move2}.${game}-lvl` || `moves.${move2}.${game}-machine` ||`moves.${move2}.${game}-tutor`)
-        .exists(true)
-        .select('name.english type abilities baseStats')
-        .sort({ _id: 1 });
-        disconnect();
-        response.status(200).json(found);
-    } else {
-        const found = await National
-        .find()
-        .where(`moves.${move1}.${game}-lvl` || `moves.${move1}.${game}-machine` ||`moves.${move1}.${game}-tutor`)
-        .exists(true)
-        .select('name.english type abilities baseStats')
-        .sort({ _id: 1 });
-        disconnect();
-        response.status(200).json(found);
+        console.log(move1)
+    if (gameList.length > 0) {
+        gameList.forEach((pokemon) => {
+            if (pokemon.moves[game]['level-up']){
+              
+                const foundMove1 = pokemon.moves[game]['level-up'].find((move) => move.name === move1);
+                if (foundMove1){
+                    console.log(foundMove1)
+                }
+              
+            }
+        })
     }
- });
- 
- module.exports = {
-     read: [connect, searchPokemon]
- }
+
+    disconnect();
+    response.status(200).json();
+});
+
+module.exports = {
+    read: [connect, searchPokemon]
+}
