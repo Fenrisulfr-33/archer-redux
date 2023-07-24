f1182ec243505920590c081fccfcb970c2f171c0
Gin Silver <Fenrisulfr.33@protonmail.com>
2023-07-24 17:55:09 -0400

    Fixed search page styling

--- frontend/src/components/MDXWrapper.jsx
+++
@@ -1,9 +0,0 @@
-export default function MDXWrapper({ children }) {
-    return(
-        // <div className={'p-prose h2-prose  h3-prose h4-prose wrap-prose a-prose code-prose'}>
-        <div className={''}>
-
-            {children}
-        </div>
-    )
-}
